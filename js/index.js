1b74d532cce731d86e4e181f9bc1e52d6875563f
programmarimran <programmarimran@gmail.com>
2025-03-03 11:27:00 +0600

    addedd a rounded button is selected

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
-//Rounded Full button capture
-function circulButtonCapture (){
-    console.log("clicked kajjj")
+const roundbtn= document.querySelectorAll(".ring-button")
+for (const ring of roundbtn){
+
+    ring.addEventListener("click",function(event){
+
+
+
+        for(let i = 0 ; i < roundbtn.length ; i++){
+            roundbtn[i].classList.remove("border-red-500")
+            roundbtn[i].classList.remove("border-transparent")
+            roundbtn[i].classList.add("border-blue-100")
+        }
+
+        // event.target.classList.remove("border-blue-500")
+        event.target.classList.add("border-red-500")
+    })
+    
+    
 }
